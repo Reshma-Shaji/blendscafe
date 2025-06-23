8120309e5e8c76776bdb665cae34c1cf4dee58eb
reshmashaji <reshmashaji31582@gmail.com>
2025-06-23 05:39:33 +0100

    first commit

--- src/app/(section)/Footer.tsx
+++ src/app/(section)/Footer.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Footer = () => {
   return (
     <div
-      className="grid grid-cols-1 md:grid-cols-3 gap-8 px-6 py-12  space-y-10 text-gray-800 uppercase"
+      className="grid grid-cols-1 md:grid-cols-3 gap-8 px-6 bg-white h-[100vh] w-full py-12 space-y-10 text-gray-800 uppercase pl-28"
       style={{ fontFamily: 'sans-serif' }}
     >
-      <div className="space-y-10 -mt-[50px]">
+      <div className="space-y-10 -mt-[20px]">
         <img src="logo.webp" alt="Logo" className="w-36 mb-4" />
-        <h2 className="font-normal text-lg mt-10">CONTACT</h2>
+        <h2 className="font-normal text-lg mt-28">CONTACT</h2>
         <h2 className="text-xl">
           69-71 London Rd, Stockton
           <br /> Heath, Warrington WA4 6SG,
@@ -18,7 +18,7 @@ const Footer = () => {
         <h2 className="text-xl">info@zaloscafe.co.uk</h2>
       </div>
 
-      <div className="space-y-7">
+      <div className="space-y-7 mt-36 pl-20">
         <h2 className="hover:underline cursor-pointer">Home</h2>
         <h2 className="hover:underline cursor-pointer">Menu</h2>
         <h2 className="hover:underline cursor-pointer">Products</h2>
@@ -26,10 +26,10 @@ const Footer = () => {
         <h2 className="hover:underline cursor-pointer">Contact Us</h2>
       </div>
 
-      <div className="space-y-10">
+      <div className="space-y-10 mt-20 pr-28">
         <h2 className="font-normal text-lg mb-2 ">Opening Hours</h2>
 
-        <div className="flex justify-between border-b pb-1">
+        <div className="flex justify-between border-b pb-1 mt-10">
           <h2>Tuesday - Thursday</h2>
           <h2>8 AM - 11 PM</h2>
         </div>
